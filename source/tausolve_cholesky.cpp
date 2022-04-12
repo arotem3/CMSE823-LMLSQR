de17c00de143dbc58be5108db3e8a8e52bfb00a8
Spencer Lee <leespen1@msu.edu>
2022-04-12 13:55:38 -0400

    Fixed tausolve_cholesky (was using b as RHS directly, instead of transpose(J)*b)

--- source/tausolve_cholesky.cpp
+++ source/tausolve_cholesky.cpp
@@ -8,22 +8,18 @@ extern "C" void dposv_(char *uplo, int *n, int *nrhs, double *A, int *lda, doubl
 // Solving (J'J + tau^2I)x = b
 Matrix tausolve_chol(tausolve_helper& helper, double tau, Matrix& b) {
     // helper.X1 = J for cholesky
-
-    char uplo = 'U'; // Store upper triangular part (although we won't use it)
-    int n = b.n_rows;
-    int nrhs = b.n_cols;
-
     Matrix J = helper.X1;
+    Matrix RHS = J.t()*b;
+    int n = RHS.n_rows;
     Matrix LHS = (J.t()*J) + (tau*tau*eye(n));
 
+    int nrhs = RHS.n_cols;
+    char uplo = 'U'; // Store upper triangular part (although we won't use it)
     // Leading dimensions should be number of rows, unless I am operating on a submatrix or something
     int lda = n;
     int ldb = n;
     int info;
     
-    Matrix x = b; // Could just give b to dposv directly, but that would overwrite
-   // old value. Maybe we want to preserve it.
-
-    dposv_(&uplo, &n, &nrhs, LHS.data(), &lda, x.data(), &ldb, &info);
-    return x;
+    dposv_(&uplo, &n, &nrhs, LHS.data(), &lda, RHS.data(), &ldb, &info);
+    return RHS;
 }
